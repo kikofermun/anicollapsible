bfcce59bab2dd4ee151cec0bdaf0db5d99cbd151
kiko <kikofdezmun@gmail.com>
2021-01-31 12:48:41 +0100

    subiendo ejemplos

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
 document.addEventListener( "DOMContentLoaded", () => {
-    document.querySelector( ".collapsible .title" ).addEventListener( "click", function(){
-        let collapsible = this.parentElement;
-        let content = collapsible.querySelector( ".content" );
-        collapsible.classList.toggle( "active" );
-        collapsible.classList.forEach( function( el ){
-            if( el.substr( 0, 2 ) == "d-" ){
-                let seconds = el.substr( 2 );
-                content.style.transitionDuration = seconds + "s";
+    document.querySelectorAll( ".collapsible .title" ).forEach( function( title ){
+        title.addEventListener( "click", function(){
+            let collapsible = this.parentElement;
+            let content = collapsible.querySelector( ".content" );
+            collapsible.classList.toggle( "active" );
+            collapsible.classList.forEach( function( el ){
+                if( el.substr( 0, 2 ) == "d-" ){
+                    let seconds = el.substr( 2 );
+                    content.style.transitionDuration = seconds + "s";
+                }
+            });
+            if( content.style.maxHeight ){
+                content.style.maxHeight = null;
+                content.style.padding = "0px 30px";
+            }else{
+                content.style.maxHeight = content.scrollHeight + "px";
+                content.style.padding = "20px 30px";
             }
         });
-        if( content.style.maxHeight ){
-            content.style.maxHeight = null;
-            content.style.padding = "0px 30px";
-        }else{
-            content.style.maxHeight = content.scrollHeight + "px";
-            content.style.padding = "20px 30px";
-        }
     });
     document.querySelectorAll( ".disableHover" ).forEach( function( disablehover ){
         let bgcolor = window.getComputedStyle( disablehover, null ).getPropertyValue( "background-color" );
